5c5691b287f168341d2cb93f91384152174a87e8
Maya Bielecki <maya.bielecki@gmail.com>
2017-06-22 13:36:44 -0400

    Fixed issues with the merge.

--- app.js
+++ app.js
@@ -12,13 +12,16 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 //Authentication for Node.js: supports log-in using username and password, facebook, google and more.
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
+const localStrategy = require('passport-local').Strategy;
 //Cors, or cross-origin resource sharing, allows javascript to interact with resources from different domains,
 //rather than being limited to only resources from the same domain. We need this because our front and back end
 //are on different ports while we develop.
 const cors = require('cors');
 
 
+//Import other folders into the app so they can be used
+const config = require("./config/database");
+const paths = require('./routes/paths');
 
 //An object-document mapper to interact with MongoDB. Allows us to build schemas (the structure of a document, expected
 // fields), //models(objects based on the schemas, instances of these are "documents" in the database),and methods to
@@ -30,43 +33,6 @@ const flash = require('connect-flash');
 const session = require('express-session');
 //---------------------------------------------------------------
 
-//Creating the express app
-const app = express();
-
-//Choosing the port number
-const port = 3000;
-
-//Starts up a server on the chosen port and send a success message
-app.listen(port, function() {
-    console.log('Server started on port ' + port);
-});
-
-//Set static folder: now the browser will automatically search here for static files such as html pages, image files and
-// css scripts.
-app.use(express.static(path.join(__dirname, 'public')));
-
-//Import other folders into the app so they can be used
-const config = require("./config/database");
-const paths = require('./routes/paths');
-
-// view engine setup
-app.set('views', __dirname+'/views');
-app.set('view engine', 'ejs');
-
-
-//BodyParser middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-
-//cookieParser middleware
-app.use(cookieParser());
-
-app.use(session({secret:'keyboard cat'}));
-
-//Connects to the routes which gives us different pages and their behaviour.
-app.use('/', paths);
-
-app.use(flash());
 
 //Connect to database
 mongoose.connect(config.database);
@@ -81,17 +47,52 @@ mongoose.connection.on('error', function(err){
     console.log("Database connection error: " + err);
 });
 
-module.exports=app;
+//---------------------------------------------------------------
+//Creating the express app
+const app = express();
 
-//Cors middleware
-app.use(cors());
+// view engine setup
+app.set('views', __dirname+'/views');
+app.set('view engine', 'ejs');
+
+//BodyParser middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//cookieParser middleware
+app.use(cookieParser());
+
+//Set static folder: now the browser will automatically search here for static files such as html pages, image files and
+// css scripts.
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({secret:'keyboard cat'}));
 
 //Passport Middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
+app.use(flash());
+
 //Import the passport strategies
 require('./config/passport')(passport);
+
+//Connects to the routes which gives us different pages and their behaviour.
+app.use('/', paths);
+
+//Cors middleware
+app.use(cors());
+
+//Choosing the port number
+const port = 3000;
+
+//Starts up a server on the chosen port and send a success message
+app.listen(port, function() {
+    console.log('Server started on port ' + port);
+});
+
+module.exports=app;
+
 
 //Use connect-flash
 
